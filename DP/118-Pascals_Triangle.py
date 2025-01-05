f1543b0d03346834166ab3c704302158a5979c2f
VictorMA18 <vmamanian@unsa.edu.pe>
2025-01-04 20:37:33 -0500

    118. Pascal's Triangle

---
+++ DP/118-Pascals_Triangle.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        memo = {}
+        
+        def get_value(row: int, col: int) -> int:
+            if col == 0 or col == row:
+                return 1
+            
+            if row < 0:
+                return 0
+                
+            key = (row, col)
+            if key in memo:
+                return memo[key]
+
+            value = get_value(row - 1, col - 1) + get_value(row - 1, col)
+            memo[key] = value
+            return value
+        
+        def generate_row(row: int) -> List[int]:
+            return [get_value(row, col) for col in range(row + 1)]
+        
+        return [generate_row(row) for row in range(numRows)]
